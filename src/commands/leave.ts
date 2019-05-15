a90df35f7f745398b121d6f889506f70d70a338b
Kevin <moistbobo@outlook.com>
2019-05-15 22:36:30 +0800

    Fix delay on tts speak Fix leave command crashing the bot

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -2,11 +2,9 @@ import CommandArgs from "../classes/CommandArgs";
 
 const leave = (args: CommandArgs) =>{
     const guildID = args.message.guild.id;
-    console.log('leave');
     if(args.bot.voiceSessions[guildID]){
-        args.bot.voiceSessions[guildID].leave().then(res=>{
-            console.log('[LEAVE COMMAND] left voice channel')
-        });
+        args.bot.voiceSessions[guildID].leave();
+        args.bot.voiceSessions[guildID] = null;
     }
 };
 
--- src/commands/tts.ts
+++ src/commands/tts.ts
@@ -1,25 +1,18 @@
 import Dectalk from "../tools/dectalk";
-import Command from "../classes/Command";
 import CommandArgs from "../classes/CommandArgs";
 
 const tts = (args: CommandArgs) =>{
     const voiceChannel = args.message.member.voiceChannel;
-
     if(voiceChannel){
-        voiceChannel.join().then(connection =>{
-            Dectalk(args.message.content, args.message.guild.id).then(res=>{
-                console.log('playing sound');
-                args.bot.voiceSessions[args.message.guild.id] = voiceChannel;
-                // args.bot.client.voiceChannel.playFile(`./${args.message.guild.id}.wav`);
-                const dispatcher = connection.playFile(`./dectalk/${args.message.guild.id}.wav`);
-            });
-
-        })
+            voiceChannel.join().then(connection =>{
+                Dectalk(args.message.content, args.message.guild.id).then(res=>{
+                    args.bot.voiceSessions[args.message.guild.id] = voiceChannel;
+                    connection.playFile(`./dectalk/${args.message.guild.id}.wav`);
+                });
+            })
     }else{
         args.message.channel.send('You need to be in a voice channel to use this command');
     }
-
-
 };
 
 export const action = tts;
