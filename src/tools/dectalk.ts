e65ffaf65ebf33b75eae04589ebc0aeddd6d9c80
Kevin <moistbobo@outlook.com>
2019-05-15 22:11:07 +0800

    Fix quotes & multi line tts not working

--- src/tools/dectalk.ts
+++ src/tools/dectalk.ts
@@ -1,14 +1,30 @@
 const Dectalk = (textToSpeak:string, serverID: string) => {
     return new Promise((resolve)=>{
-        const execFile = require('child_process').execFile;
-        execFile('./say.exe', ['-w', `./dectalk/${serverID}.wav`, textToSpeak], (err, stdout, sterr)=>{
-            console.log('[CALLDECTALK FUNCTION]');
-            console.log(err);
-            console.log(stdout);
-            console.log(sterr);
+        console.log(process.platform);
+        if(process.platform === 'linux'){
+            const exec = require('child_process').exec;
 
-            resolve(serverID);
-        });
+            exec(`DISPLAY:=0.0 wine ./say.exe -w ./dectalk/${serverID}.wav "${textToSpeak}"`, (err, stdout, sterr)=>{
+                console.log('[CALLDECTALK FUNCTION]');
+                console.log(err);
+                console.log(stdout);
+                console.log(sterr);
+
+                resolve(serverID);
+            })
+
+        }else if (process.platform === 'win32'){
+            const execFile = require('child_process').execFile;
+            execFile('./say.exe', ['-w', `./dectalk/${serverID}.wav`, textToSpeak], (err, stdout, sterr)=>{
+                console.log('[CALLDECTALK FUNCTION]');
+                console.log(err);
+                console.log(stdout);
+                console.log(sterr);
+
+                resolve(serverID);
+            });
+        }
+
 
     });
 };
