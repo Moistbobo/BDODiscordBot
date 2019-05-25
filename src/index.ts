74cbc69c84f69455a9b26f5fdb7a6ab3c3f6f05b
Kevin <moistbobo@outlook.com>
2019-05-25 23:43:36 +0800

    Dockerization
    Add docker file and .dockerignore
    
    Add new npm build script that will build typescript and copy docker files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import * as token from './token.json'
 import * as config from './config.json'
-
-// const baseString = 'hello {0} my name is {1} and i am here to {2}';
-// const values = ['everyone', 'bob' , 'destroy everyone and all '];
-// console.log(replace(baseString, values));
-//
-// console.log('hello world');
-//
-// Dectalk(replace(baseString, values), '1235');
 
 import Bot from './classes/Bot';
 
-const bot = new Bot('.',token.token, config);
+!process.env.bobobotToken ? console.log('Make sure bobobotToken exists in your system variables.\n' +
+    'If running from docker, set the -e bobobotToken=$[YOUR_TOKEN_HERE]') :
+    console.log('Logging into token:\n', process.env.bobobotToken);
+const bot = new Bot('.', process.env.bobobotToken, config);
