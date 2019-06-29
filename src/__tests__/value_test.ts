dd077b12f3f5f4c495af48693ff1666f5c7d94b1
Kevin <moistbobo@outlook.com>
2019-06-29 23:29:11 +0800

    Prepare from tests for value command

---
+++ src/__tests__/value_test.ts
@@ -0,0 +1,31 @@
+import {valueTest} from '../commands/bdo/value';
+
+describe('Value Command', () => {
+
+    describe('toMachineReadable', () => {
+        test.each`
+    input     | expectedResult
+    ${'9.6bil'}  |  ${9600000000}
+    ${'9.6b'} |     ${9600000000}
+    ${'1k'} | ${1000}
+    ${'10k'} | ${10000}
+    ${'1mil'} | ${1000000}
+    ${'10mil'} | ${10000000}
+    ${'100mil'} |   ${100000000}
+    ${'1m'} |  ${1000000}
+    ${'10m'} | ${10000000}
+    ${'100m'} | ${100000000}
+  `('converts $input to $expectedResult', ({input, expectedResult}) => {
+            expect(valueTest.toMachineReadable(input)).toBe(expectedResult)
+        });
+    });
+
+    describe('toHumanReadable', () => {
+        test.each`
+   input | expectedResult
+   ${1000000} | ${'1mil'}
+   `('converts $input to $expectedResult', ({input, expectedResult}) => {
+            expect(valueTest.toHumanReadable(input)).toBe(expectedResult);
+        })
+    });
+});
