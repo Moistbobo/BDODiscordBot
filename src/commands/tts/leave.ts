5e7f48e8be24cdd66ff828190b810caabd560918
Kevin <moistbobo@outlook.com>
2019-06-30 23:16:08 +0800

    Added: Leave command now checks if the command was run in the gtts channel

--- src/commands/tts/leave.ts
+++ src/commands/tts/leave.ts
@@ -1,11 +1,28 @@
 import CommandArgs from "../../classes/CommandArgs";
+import GTTSChannel from "../../models/gttsChannel";
 
-const leave = (args: CommandArgs) =>{
-    const guildID = args.message.guild.id;
-    if(args.bot.voiceSessions[guildID]){
-        args.bot.voiceSessions[guildID].leave();
-        args.bot.voiceSessions[guildID] = null;
-    }
+const checkGTTSChannel = (serverID) => {
+    return GTTSChannel.findOne({serverID});
+};
+
+const leave = (args: CommandArgs) => {
+
+    checkGTTSChannel(args.message.guild.id)
+        .then((gttsChannel) => {
+            if (gttsChannel.channelID !== args.message.channel.id) {
+                return args.sendErrorEmbed({contents: 'This channel is not gtts enabled'});
+            }
+            else if(!gttsChannel){
+                return args.sendErrorEmbed({contents:'tts channel not set for this server'});
+            }
+
+            const guildID = args.message.guild.id;
+
+            if (args.bot.voiceSessions[guildID]) {
+                args.bot.voiceSessions[guildID].leave();
+                args.bot.voiceSessions[guildID] = null;
+            }
+        });
 };
 
 export const action = leave;
