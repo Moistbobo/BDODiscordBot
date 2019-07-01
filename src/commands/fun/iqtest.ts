a81482aedaa2e0aa30c9182db3041067da77585c
Kevin <moistbobo@outlook.com>
2019-07-02 00:06:18 +0800

    Added: Comments to iqtest command

--- src/commands/fun/iqtest.ts
+++ src/commands/fun/iqtest.ts
@@ -3,6 +3,60 @@ import Images from "../../resources/images";
 import Jimp from 'jimp';
 import IQTestResult from "../../models/iqtestResult";
 
+const iqTest = (args: CommandArgs) => {
+    let iq = 0;
+    const userID = args.message.author.id;
+    args.message.channel.startTyping();
+
+    // Check mongodb if user has run the "iqtest" within the last 24 hours (86400 seconds)
+    IQTestResult.findOne({userID})
+        .then((iqTestResult) => {
+            if (iqTestResult) {
+                const currentTime = Date.now() / 1000;
+                console.log(currentTime - iqTestResult.lastUpdate);
+                return {timeDifference: (currentTime - iqTestResult.lastUpdate), iqTestResult};
+            } else {
+                return {timeDifference: 86400};
+            }
+        })
+        .then((res) => {
+            // If delta from lastUpdated time is less than 24hours, break out of promise chain
+            if (res.timeDifference < 86400) {
+                // 24hour limit on iqtest command
+                sendIQMessage(args.message.channel, res.iqTestResult.iq, args.message.author.id);
+                throw new Error('IQ Cooldown not met');
+            } else {
+                // Otherwise, load images and continue
+                return loadImages();
+            }
+        })
+        .then((images) => {
+            // Generate random "iq" value and plot marker on graph
+            const iqChart = images[0];
+            const iqMark = images[1];
+            const xPlot = plotIQMark(400);
+            iq = mapPlotPointToIQ(xPlot, 400, 0, iqChart.getWidth(), 0);
+            return iqChart.composite(iqMark, xPlot, 5);
+        })
+        .then((compositedImage) => {
+            // Check if folder exists, and create it if it doesn't and then write the picture to a file
+            makeIQFolderIfNotExists();
+            return compositedImage.writeAsync(`./iqtest/${args.message.author.id}.png`);
+        })
+        .then(() => {
+            return saveIQTestResult(args.message.author.id, iq);
+        })
+        .then(() => {
+            return sendIQMessage(args.message.channel, iq, args.message.author.id);
+        })
+        .catch((err)=>{
+            console.log(err.toString());
+        })
+        .finally(()=>{
+            args.message.channel.stopTyping();
+        })
+};
+
 const loadImages = () => {
     return Promise.all([
         Jimp.read(Images.iqTest.iqChart)
@@ -17,19 +71,32 @@ const loadImages = () => {
 };
 
 /**
- * Make /gtts directory to store tts files if it doesn't exist
+ * Create folder to hold the generated images
  */
-const makeIQFolderIgNotExists = () => {
+const makeIQFolderIfNotExists = () => {
     const fs = require('fs');
     if (!fs.existsSync('./iqtest')) {
         fs.mkdirSync('./iqtest');
     }
 };
 
+/**
+ * Generate random x value to plot the marker
+ * @param xMax
+ */
 const plotIQMark = (xMax: number) => {
     return Math.floor(Math.random() * Math.floor(xMax));
 };
 
+/**
+ * Apply linear transformation on the plot point so that it is mapped correctly on the graph
+ * @param xPlot
+ * @param iqMax
+ * @param iqMin
+ * @param imageMax
+ * @param imageMin
+ */
+// This is only necessary if the scale on the image does not match the actual dimensions
 const mapPlotPointToIQ = (xPlot: number, iqMax: number, iqMin: number, imageMax: number, imageMin: number) => {
     return ((xPlot - imageMin) / (imageMax - imageMin)) * (iqMax - iqMin) + iqMin;
 };
@@ -42,19 +109,6 @@ const saveIQTestResult = (userID: string, iq: number): Promise<any> => {
     return newIQTestResult.save();
 };
 
-const convertToHumanReadable = (seconds: number) => {
-    let d = seconds;
-    d = Number(d);
-    let h = Math.floor(d / 3600);
-    let m = Math.floor(d % 3600 / 60);
-    let s = Math.floor(d % 3600 % 60);
-
-    let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return hDisplay + mDisplay + sDisplay;
-};
-
 const sendIQMessage = (channel: any, iq: number, authorID: string) => {
     return channel.send({
         embed: {
@@ -65,56 +119,6 @@ const sendIQMessage = (channel: any, iq: number, authorID: string) => {
             name: 'iqtest.png'
         }]
     });
-};
-
-const iqTest = (args: CommandArgs) => {
-    // First check if the user is allowed to update
-    const userID = args.message.author.id;
-    args.message.channel.startTyping();
-
-    let iq = 0;
-    // Promise chaining is cool and all but I should really reduce this
-    IQTestResult.findOne({userID})
-        .then((iqTestResult) => {
-            if (iqTestResult) {
-                const currentTime = Date.now() / 1000;
-                console.log(currentTime - iqTestResult.lastUpdate);
-                return {timeDifference: (currentTime - iqTestResult.lastUpdate), iqTestResult};
-            } else {
-                return {timeDifference: 86400};
-            }
-        })
-        .then((res) => {
-            if (res.timeDifference < 86400) {
-                sendIQMessage(args.message.channel, res.iqTestResult.iq, args.message.author.id);
-                throw new Error('IQ Cooldown not met');
-            } else {
-                return loadImages();
-            }
-        })
-        .then((images) => {
-            const iqChart = images[0];
-            const iqMark = images[1];
-            const xPlot = plotIQMark(400);
-            iq = mapPlotPointToIQ(xPlot, 400, 0, iqChart.getWidth(), 0);
-            return iqChart.composite(iqMark, xPlot, 5);
-        })
-        .then((compositedImage) => {
-            makeIQFolderIgNotExists();
-            return compositedImage.writeAsync(`./iqtest/${args.message.author.id}.png`);
-        })
-        .then(() => {
-            return saveIQTestResult(args.message.author.id, iq);
-        })
-        .then(() => {
-            args.message.channel.stopTyping();
-            return sendIQMessage(args.message.channel, iq, args.message.author.id);
-
-        })
-        .catch((err)=>{
-            console.log(err.toString());
-            args.message.channel.stopTyping();
-        });
 };
 
 export const action = iqTest;
