9c290635753c7fc6287ff22a8b34d7d3dbbfcdfd
Kevin <moistbobo@outlook.com>
2019-07-10 18:26:36 +0800

    Fix extra comma in heal strings

--- src/resources/strings/heal.ts
+++ src/resources/strings/heal.ts
@@ -2,7 +2,7 @@ const healStrings = {
     hpFull: '{0}\'s HP is already full.',
     healSuccess:'{3} healed for {0}HP.\n\n{3} is at {1}/{2}HP',
     timeUntilHeal: '{1}, {0} seconds until next heal.',
-    userIsDead: '{0}, is already dead.'
+    userIsDead: '{0} is already dead.'
 };
 
 export default healStrings ;
