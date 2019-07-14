82b1a8c3ca86a288d171929c2266adc398b00cc2
Kevin <moistbobo@outlook.com>
2019-07-15 00:45:53 +0800

    Refactor createitem command

--- src/commands/rpgAdmin/createitem.ts
+++ src/commands/rpgAdmin/createitem.ts
@@ -1,7 +1,4 @@
 import CommandArgs from "../../classes/CommandArgs";
-import {FindOrCreateNewRPGCharacter} from "../../models/rpg/RPGCharacter";
-import ItemFactory from "../../models/rpg/Factories/ItemFactory";
-import {ItemTypes} from "../../models/rpg/Item";
 import RPGTools from "../../tools/rpg/RPGTools";
 
 const createItem = (args: CommandArgs) => {
@@ -11,25 +8,6 @@ const createItem = (args: CommandArgs) => {
 
     if (!args.message.content || args.message.member.id !== '80300923351465984') return;
     console.log(qty);
-    // Promise.all([FindOrCreateNewRPGCharacter(userID),
-    //     ItemFactory.CreateNewItem(itemID, count || 1)])
-    //     .then((res) => {
-    //         const rpgCharacter = res[0];
-    //         const newItem = res[1];
-    //
-    //         // if(!newItem) throw new Error(`Could not find ItemID ${itemID} in database`);
-    //
-    //         let itemIndex = rpgCharacter.inventory.findIndex((x) => x.itemID === newItem.itemID);
-    //         if (newItem.itemType !== ItemTypes[0] && itemIndex !== -1) {
-    //             console.log('Item is stackable, adding it to the players inventory');
-    //             rpgCharacter.inventory[itemIndex].qty += newItem.qty;
-    //         } else {
-    //             console.log('Adding item to player inventory');
-    //             rpgCharacter.inventory.push(newItem);
-    //         }
-    //
-    //         return rpgCharacter.save();
-    //     })
     RPGTools.AddItemToUserInventory(userID, itemID, qty)
         .catch((err) => {
             console.log(err.toString());
