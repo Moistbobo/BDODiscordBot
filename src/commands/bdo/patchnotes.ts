a03b1bb1e38154bd62e079b08cde559688f37afe
Kevin <moistbobo@outlook.com>
2019-07-15 08:39:13 +0800

    Add history argument to patchnotes (maximum 10)

--- src/commands/bdo/patchnotes.ts
+++ src/commands/bdo/patchnotes.ts
@@ -5,6 +5,10 @@ const rssFeedUrl = "https://community.blackdesertonline.com/index.php?forums/pat
 
 const patchnotes = (args: CommandArgs) => {
 
+    const history = Math.min((parseFloat(args.message.content) || 1), 10);
+
+    console.log(history);
+
     const parser = require('fast-xml-parser');
     fetch(rssFeedUrl)
         .then((result) => {
@@ -15,11 +19,17 @@ const patchnotes = (args: CommandArgs) => {
             const tobj = parser.getTraversalObj(xml);
             const json = parser.convertToJson(tobj);
 
-            const pn = json.rss.channel.item[0];
+            const pn = json.rss.channel.item;
+
+            let pnString = '';
+
+            for (let i = 0; i < history; i++) {
+                pnString += `${pn[i].title}\n${pn[i].link} \n\n`
+            }
 
             let embedArgs = {
                 title: 'Patch Notes',
-                contents: `${pn.title}\n${pn.link} `
+                contents: pnString
             };
             args.sendOKEmbed(embedArgs);
         })
