e239d6b965f10108fbcd803ee06487e493f9bd2a
Kevin <moistbobo@outlook.com>
2019-07-20 20:26:53 +0800

    Add ability to heal others (60% efficiency)

--- src/commands/rpg/heal.ts
+++ src/commands/rpg/heal.ts
@@ -5,8 +5,9 @@ import {CanHeal, FindOrCreateRPGTimer} from "../../models/rpg/RPGTimer";
 import Timers from "../../resources/Timers";
 import {FindOrCreateRPGServerStats, IsChannelRPGEnabled} from "../../models/rpg/RPGServerStats";
 import RPGTools from "../../tools/rpg/RPGTools";
+import {User} from 'discord.js';
 
-const heal = (args: CommandArgs) => {
+const healSelf = (args: CommandArgs) => {
     const userID = args.message.author.id;
 
     IsChannelRPGEnabled(args)
@@ -64,6 +65,82 @@ const heal = (args: CommandArgs) => {
         .catch((err) => {
             console.log(err.toString());
         })
+};
+
+const healOther = (args: CommandArgs, targetUser: User) => {
+    const userID = args.message.author.id;
+    const targetUserID = targetUser.id;
+
+    IsChannelRPGEnabled(args)
+        .then((res) => {
+            if (!res) {
+                args.message.react('❌');
+                throw new Error('Non RPG Channel')
+            }
+            return Promise.all([FindOrCreateRPGTimer(userID), FindOrCreateNewRPGCharacter(userID), FindOrCreateNewRPGCharacter(targetUserID), FindOrCreateRPGServerStats(args.message.guild.id)])
+
+        })
+        .then((res) => {
+            let [rpgTimer, healingCharacter, targetCharacter, rpgServerStats] = res;
+
+            if (!CanHeal(rpgTimer.lastHeal)) {
+                const timeToHeal = Timers.rpg.healCD - (Date.now() / 1000 - rpgTimer.lastHeal);
+                args.sendErrorEmbed({
+                    contents: replace(args.strings.heal.timeUntilHeal, [timeToHeal.toFixed(0),
+                        args.message.author.username])
+                });
+                throw new Error('Heal is on CD');
+            }
+
+            if (targetCharacter.hitpoints.current === targetCharacter.hitpoints.max) {
+                args.sendErrorEmbed({
+                    contents: replace(args.strings.heal.hpFull,
+                        [targetUser.username])
+                });
+                throw new Error('User is at max HP');
+            }
+
+            if (targetCharacter.hitpoints.current <= 0) {
+                args.sendErrorEmbed({contents: replace(args.strings.heal.userIsDead, [targetUser.username])});
+                throw new Error('User is dead');
+            }
+
+            const healAmount =
+                Math.floor(RPGTools.HealCalculation(healingCharacter.stats.int, healingCharacter.stats.bal) * 0.60);
+
+            targetCharacter.hitpoints.current = Math.min(targetCharacter.hitpoints.max, targetCharacter.hitpoints.current + healAmount);
+            rpgTimer.lastHeal = Date.now() / 1000;
+            rpgServerStats.heals++;
+
+            args.sendOKEmbed({
+                footer: args.strings.heal.healOtherFooter,
+                contents: replace(args.strings.heal.healOther, [
+                    args.message.author.username,
+                    targetUser.username,
+                    healAmount,
+                    targetCharacter.hitpoints.current,
+                    targetCharacter.hitpoints.max,
+                ])
+            });
+
+            return Promise.all([targetCharacter.save(), rpgTimer.save(), rpgServerStats.save()]);
+        })
+        .catch((err) => {
+            console.log(err.toString());
+        })
+};
+
+const heal = (args: CommandArgs) => {
+    const targetUser = args.bot.getFirstMentionedUserID(args.message);
+    console.log(targetUser);
+
+    if (targetUser) {
+        healOther(args, targetUser);
+    } else {
+        healSelf(args);
+    }
+
+
 };
 
 export const action = heal;
--- src/resources/strings/en/commands/healStrings.ts
+++ src/resources/strings/en/commands/healStrings.ts
@@ -2,7 +2,9 @@ const healStrings = {
     hpFull: '{0}\'s HP is already full.',
     healSuccess:'{3} healed for {0}HP.\n\n{3} is at {1}/{2}HP',
     timeUntilHeal: '{1}, {0} seconds until next heal.',
-    userIsDead: '{0} is already dead.'
+    userIsDead: '{0} is already dead.',
+    healOther: `{0} healed {1} for {2}HP\n\n{1} is at {3}/{4}HP`,
+    healOtherFooter: `Healing others is less effective than healing yourself`
 };
 
 export default healStrings ;
