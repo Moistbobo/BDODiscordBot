146662fea50af1712093d67b6238cc87f833316f
Kevin <moistbobo@outlook.com>
2019-07-24 00:11:02 +0800

    Remove unused commands

--- src/commands/rpg/rpghelp.ts
+++
@@ -1,9 +0,0 @@
-import CommandArgs from "../../classes/CommandArgs";
-
-const rpghelp = (args: CommandArgs) =>{
-    args.sendOKEmbed({
-        contents: `Temporary help screen for RPG commands:\n.attack\n.respawn\n.status\n.heal`
-    })
-};
-
-const action = rpghelp;
